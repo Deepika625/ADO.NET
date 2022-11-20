6795c0d88fdf9d7aa7977b212f220e0b350fec8b
Deepika625 <118673564+Deepika625@users.noreply.github.com>
2022-11-20 15:16:09 +0000

    ADO.NET assignment

---
+++ Create.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    internal class p1
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"data source=DeepikaVM;inital catlog=Empolyee;Integrated security=true";
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+
+            Console.WriteLine("Please Enter Employee Id : ");
+            int EmpId = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nPlease Enter Employee First Name : ");
+            String EmpFName = Console.ReadLine();
+
+            Console.WriteLine("\nPlease Enter Employee Last Name  : ");
+            String EmpLname = Console.ReadLine();
+
+            Console.WriteLine("\nPlease Enter Employee Salary : ");
+            int EmpSalary = Convert.ToInt32(Console.ReadLine());
+
+
+            string insertQuery = "insert into Employee_tb(EmpID, Firstname, Lastname, Salary) Values('" + EmpId + "','" + EmpFName + "','" + EmpLname + "','" + EmpSalary + "')";
+
+
+            SqlCommand insertCommand = new SqlCommand(insertQuery, sqlConnection);
+            insertCommand.ExecuteNonQuery();
+            Console.WriteLine("Data Inserted");
+            sqlConnection.Close();
+        }
+    }
+}
